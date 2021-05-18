24e2854125db9d99863fb6aee3fd93e7aa9c81de
reid harward <reid.harward@gmail.com>
2021-05-18 16:29:54 -0400

    changed the lable on a button and fixed the formatting of the search return

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const  request  = require('request');
+const request = require('request');
 const cheerio = require('cheerio');
 const express = require('express');
 var cors = require('cors');
@@ -20,8 +20,8 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
   }
   next();
 });
@@ -37,17 +37,17 @@ var Datastore = require('nedb');
 
 //app.listen(7000 , "dev.citynet.net"|"45.76.18.92", () =>console.log('listening at 7000'));
 //app.listen(7000 , "dev.citynet.net", () =>console.log('listening at 7000'));
-app.use(express.static('public',cors()));
-app.use(express.static('public/data',cors()));
-app.use(express.static('http://127.0.0.1:7000/'+ '/public',cors()));
-app.use(express.static('http://localhost:7000'+ '/public',cors()));
+app.use(express.static('public', cors()));
+app.use(express.static('public/data', cors()));
+app.use(express.static('http://127.0.0.1:7000/' + '/public', cors()));
+app.use(express.static('http://localhost:7000' + '/public', cors()));
 ////app.use(express.static('public',cors()));
 ////app.use(express.static('public/data',cors()));
 //app.use(express.static('http://127.0.0.1:7000/'+ '/public',cors()));
 ////app.use(express.static('http://localhost:7000'+ '/public',cors()));
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: '10mb' }));
 //app.listen(7000 , "dev.citynet.net"|"45.76.18.92", () =>console.log('listening at 7000'));
-app.listen(7000 , "dev.citynet.net"|"localhost", () =>console.log('listening at 7000'));
+app.listen(7000, "dev.citynet.net" | "localhost", () => console.log('listening at 7000'));
 //const database = new Datastore({filename: __dirname +'database.db', autoload: true});
 //const topiclist = new Datastore({filename: __dirname+'topiclist.db', autoload: true});
 const database = new Datastore('database.db');
@@ -58,96 +58,96 @@ database.loadDatabase(function (error) { if (error) { console.log('FATAL: local 
 //database.loadDatabase();
 var http = require('http');
 var fs = require('graceful-fs');
-app.get('/getsubtitles', (request,response) => {
-    database.find({},(err,data)=>{
-      if (err){
-        response.end();
-        return;
-      }
-      response.json(data);
-    });
+app.get('/getsubtitles', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
 });
 
 
-app.post ('/getsubtitles', cors(), (request1,response) =>  {
+app.post('/getsubtitles', cors(), (request1, response) => {
 
-let Yserial = "";
-let topic = "";
-Yserial = request1.body.Yserial;
-topic = request1.body.topicsSelect;
- let Zserial = Yserial.split("="); //this splits the id from the right hand side of the youtube address
-console.log('Zserial[1] = '+Zserial[1]+' topic = '+topic);
-var getSubtitles = require('youtube-captions-scraper').getSubtitles;
-//const Zserial = Zserial[1];
-getSubtitles({
-  videoID: Zserial[1], // youtube video id
-  lang: 'en' // default: `en`
-}).then(function(captions, topic) {
-  captions.push(String(Zserial[1]));
-  // database.insert({captions},{topic});
-   database.insert({captions});
-  // database.insert({topic});
-   console.log('in here');
-});
-response.json('finished grabbing timed text')
+  let Yserial = "";
+  let topic = "";
+  Yserial = request1.body.Yserial;
+  topic = request1.body.topicsSelect;
+  let Zserial = Yserial.split("="); //this splits the id from the right hand side of the youtube address
+  console.log('Zserial[1] = ' + Zserial[1] + ' topic = ' + topic);
+  var getSubtitles = require('youtube-captions-scraper').getSubtitles;
+  //const Zserial = Zserial[1];
+  getSubtitles({
+    videoID: Zserial[1], // youtube video id
+    lang: 'en' // default: `en`
+  }).then(function (captions, topic) {
+    captions.push(String(Zserial[1]));
+    // database.insert({captions},{topic});
+    database.insert({ captions });
+    // database.insert({topic});
+    console.log('in here');
+  });
+  response.json('finished grabbing timed text')
 })
 
 
 //below is the function to remove the database totally.  
 app.post('/dropdatabase', cors(), (request1, response) => {
-console.log('DROPPING DATABASE')
-  database.remove({ }, { multi: true }, function (err, numRemoved) {
+  console.log('DROPPING DATABASE')
+  database.remove({}, { multi: true }, function (err, numRemoved) {
     database.loadDatabase(function (err) {
       // done
     });
   });
 })
 
-app.post('/droprecord',  cors(), (request1, response) => {
-  
+app.post('/droprecord', cors(), (request1, response) => {
+
   let currentRecord = "";
   currentRecord = (request1.body.data);
-  console.log('currentRecord ='+currentRecord);
-  database.remove({ _id: currentRecord }, {}, function (err, numRemoved){
+  console.log('currentRecord =' + currentRecord);
+  database.remove({ _id: currentRecord }, {}, function (err, numRemoved) {
 
-  console.log('DROPPED RECORD');
-})
-database.persistence.compactDatafile();
-response.json('dropped record');
-  });
-
-app.post('/addtopic',  cors(), (request1,response) =>{
-    let currentRecord = "";
-    currentRecord = request1.body.Eserial;
-    console.log('current Record = '+ currentRecord);
-    topiclist.insert({currentRecord}, function (err, currentRecord){
-      console.log('ADDED RECORD');
-      let data = JSON.stringify(currentRecord);
-      response.send(data);
-      topiclist.persistence.compactDatafile()
-     })
-     //return (null, currentRecord);
-    
+    console.log('DROPPED RECORD');
   })
+  database.persistence.compactDatafile();
+  response.json('dropped record');
+});
 
-  app.get('/populatetopic',  cors(), (request,response) => {
+app.post('/addtopic', cors(), (request1, response) => {
+  let currentRecord = "";
+  currentRecord = request1.body.Eserial;
+  console.log('current Record = ' + currentRecord);
+  topiclist.insert({ currentRecord }, function (err, currentRecord) {
+    console.log('ADDED RECORD');
+    let data = JSON.stringify(currentRecord);
+    response.send(data);
+    topiclist.persistence.compactDatafile()
+  })
+  //return (null, currentRecord);
+
+})
+
+app.get('/populatetopic', cors(), (request, response) => {
   console.log('inside the populate topic function in here to post the video');
-    let currentRecord = "";
-    currentRecord = request.body.data;
-    
-      topiclist.find({},(err,data)=>{
-        if (err){
-          response.end();
-          return;
-        }else{
+  let currentRecord = "";
+  currentRecord = request.body.data;
+
+  topiclist.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    } else {
       response.send(data);
-       console.log('DATA = '+ data) //take this out to getrid of the long console.log when you post a video
-        }//response = data;
-       //database.count({}, function (err, count) {
-        //response.end();
-        //console.log('Count = '+ database.count );})
-      });
+      console.log('DATA = ' + data) //take this out to get rid of the long console.log when you post a video
+    }//response = data;
+    //database.count({}, function (err, count) {
+    //response.end();
+    //console.log('Count = '+ database.count );})
   });
+});
 
  // })
    // });
